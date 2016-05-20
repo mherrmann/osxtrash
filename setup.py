9200715b0167fe72926d68a52f58a3cfb0ea9d55
Michael Herrmann <michael@herrmann.io>
2016-05-20 12:27:43 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ impl = Extension(
 
 setup(
 	name='osxtrash',
-	version='1.0',
+	version='1.1',
 	description='Send files to the Trash on OS X (incl. "Put Back" support).',
 	long_description=
 		'Send files to the Trash on OS X (incl. "Put Back" support).' + 
